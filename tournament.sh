00739d682c02c90464481243650887f3fb66467d
Max <max.dupenois@gmail.com>
2017-03-08 17:21:27 +0000

    Tournament script updates

--- tournament.sh
+++ tournament.sh
@@ -2,7 +2,7 @@
 set -e #exit on error
 
 ME="swarming"
-OPPONENTS=("samwho" "ben.one")
+OPPONENTS=("samwho" "ben.one" "examplefuncsplayer")
 MAPS=("Arena" "Alone")
 
 echo -e "\033[0;35mStarting Tournament\033[0m"
@@ -10,14 +10,23 @@ echo -e "\033[0;35m-------------------\033[0m"
 for o in ${OPPONENTS[@]}; do
   for m in ${MAPS[@]}; do
     echo -e "\033[0;37mVs. ${o} on ${m}\033[0m"
-    COMMAND="gradle --offline run -PteamA=swarming -PteamB=${o} -Pmaps=${m}"
-    RESULT=`${COMMAND} | grep -E -A 1 '\[server\].*wins.*'`
-    CLEAN_RESULT=`echo ${RESULT} | sed -E 's/\[server\] *//g'`
-    if [[ $CLEAN_RESULT == *${ME}* ]]; then
-      echo -e "\033[0;36m  ${CLEAN_RESULT}\033[0m"
+    COMMAND_A="gradle --offline run -PteamA=swarming -PteamB=${o} -Pmaps=${m}"
+    COMMAND_B="gradle --offline run -PteamB=swarming -PteamA=${o} -Pmaps=${m}"
+    RESULT_A=`${COMMAND_A} | grep -E -A 1 '\[server\].*wins.*'`
+    RESULT_B=`${COMMAND_B} | grep -E -A 1 '\[server\].*wins.*'`
+    CLEAN_RESULT_A=`echo ${RESULT_A} | sed -E 's/\[server\] *//g'`
+    CLEAN_RESULT_B=`echo ${RESULT_B} | sed -E 's/\[server\] *//g'`
+    if [[ $CLEAN_RESULT_A == *${ME}* ]]; then
+      echo -e "\033[0;36m  A: ${CLEAN_RESULT_A}\033[0m"
     else
-      echo -e "\033[0;31m  ${CLEAN_RESULT}\033[0m"
+      echo -e "\033[0;31m  A: ${CLEAN_RESULT_A}\033[0m"
+      echo -e "\033[0;31m     ${COMMAND_A}\033[0m"
     fi
-    echo -e "\033[0;32m  RERUN: ${COMMAND}\033[0m"
+    if [[ $CLEAN_RESULT_B == *${ME}* ]]; then
+      echo -e "\033[0;36m  B: ${CLEAN_RESULT_B}\033[0m"
+    else
+      echo -e "\033[0;31m  B: ${CLEAN_RESULT_B}\033[0m"
+      echo -e "\033[0;31m     ${COMMAND_B}\033[0m"
+    fi
   done
 done

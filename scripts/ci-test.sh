0aa8542e87265f5b6c652cbff0fbce2d0236b5b6
Max <max.dupenois@gmail.com>
2017-03-10 14:55:46 +0000

    Cleaned up the script files and hopefully will now prevent the ci script from timing out

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -6,6 +6,7 @@ SLACK_CHANNEL="#battlecode-results"
 
 
 BOTS=("swarming" "samwho" "ben.one" "rybots")
+#BOTS=("swarming" "examplefuncsplayer")
 MAPS=("Arena" "Alone")
 RESULTS=""
 
@@ -40,29 +41,77 @@ ln -s ../others/rybots/src/rybots ./rybots
 cd ..
 echo "Running tournament"
 
+function buildMatchCommand(){
+  team1=$1
+  team2=$2
+  map=$3
+  echo "./gradlew runQuiet -PteamA=${team1} -PteamB=${team2} -Pmaps=${map}"
+  return 0
+}
+
+function extractTime(){
+  result=$1
+  echo $(echo -e $result | grep -Eo 'Total time: .* secs')
+  return 0
+}
+
+function resultSummary(){
+  result=$1
+  timeTaken=$(extractTime "$result")
+  lines=$(echo "${result}" | grep -E -A 1 '\[server\].*wins.*')
+  summary=$(echo $lines | sed -E 's/\[server\] *//g')
+  echo "${summary} (${timeTaken})"
+  return 0
+}
+spin='-\|/'
+
 for b1 in ${BOTS[@]}; do
   RESULTS="${RESULTS}\n\r*Testing ${b1} _(Team A)_*\n\r*-------------*"
   for b2 in ${BOTS[@]}; do
     if [[ $b1 == $b2 ]]; then continue; fi
     for m in ${MAPS[@]}; do
       echo "${b1} vs ${b2} on ${m}"
-      COMMAND="./gradlew runQuiet -PteamA=${b1} -PteamB=${b2} -Pmaps=${m}"
-      RESULT=`${COMMAND} | grep -E -A 1 '\[server\].*wins.*'`
-      CLEAN_RESULT=`echo ${RESULT} | sed -E 's/\[server\] *//g'`
-      if [[ $CLEAN_RESULT == *${b1}* ]]; then
-        RESULTS="${RESULTS}\n\r    :smile: *WIN * against ${b2} on ${m} :: ${CLEAN_RESULT}"
+      COMMAND=$(buildMatchCommand "$b1" "$b2" "$m")
+      tmp=$(mktemp)
+      resultTmp=$(mktemp)
+      ($COMMAND >"$resultTmp"; echo $? >"$tmp") &
+      pid=$!
+      i=0
+      while kill -0 $pid 2>/dev/null
+      do
+        i=$(( (i+1) %4 ))
+        printf "\r[playing] ${spin:$i:1}"
+        sleep .1
+      done
+      echo -e "\rGame Complete"
+      read ret <"$tmp"
+      RESULT="$(cat $resultTmp)"
+      if [[ $ret != 0 ]]; then return $ret; fi
+
+      SUMMARY=$(resultSummary "$RESULT")
+      if [[ $SUMMARY == *${b1}* ]]; then
+        RESULTS="${RESULTS}\n\r    :smile: *WIN * against ${b2} on ${m} :: ${SUMMARY}"
       else
-        RESULTS="${RESULTS}\n\r    :rage: *LOSS* against ${b2} on ${m} :: ${CLEAN_RESULT}"
+        RESULTS="${RESULTS}\n\r    :rage: *LOSS* against ${b2} on ${m} :: ${SUMMARY}"
       fi
+
+      #COMMAND="./gradlew runQuiet -PteamA=${b1} -PteamB=${b2} -Pmaps=${m}"
+      #RESULT=`${COMMAND} | grep -E -A 1 '\[server\].*wins.*'`
+      #CLEAN_RESULT=`echo ${RESULT} | sed -E 's/\[server\] *//g'`
+      #if [[ $CLEAN_RESULT == *${b1}* ]]; then
+      #  RESULTS="${RESULTS}\n\r    :smile: *WIN * against ${b2} on ${m} :: ${CLEAN_RESULT}"
+      #else
+      #  RESULTS="${RESULTS}\n\r    :rage: *LOSS* against ${b2} on ${m} :: ${CLEAN_RESULT}"
+      #fi
     done
   done
 done
 echo -e $RESULTS
 
-payload="payload={\"channel\":\"${SLACK_CHANNEL}\",
-\"username\":\"Battlecode Tournament Result\",
-\"icon_emoji\":\":robot_face:\",
-\"text\":\"${RESULTS}\"
-}"
-
-curl -X POST --data-urlencode "${payload}" ${WEBHOOK_URL}
+#payload="payload={\"channel\":\"${SLACK_CHANNEL}\",
+#\"username\":\"Battlecode Tournament Result\",
+#\"icon_emoji\":\":robot_face:\",
+#\"text\":\"${RESULTS}\"
+#}"
+#
+#curl -X POST --data-urlencode "${payload}" ${WEBHOOK_URL}
--- scripts/tournament.sh
+++ scripts/tournament.sh
@@ -3,30 +3,80 @@ set -e #exit on error
 
 ME="swarming"
 OPPONENTS=("samwho" "ben.one" "rybots" "examplefuncsplayer")
+#OPPONENTS=("examplefuncsplayer")
 MAPS=("Arena" "Alone")
+
+function extractTime(){
+  result=$1
+  echo $(echo -e $result | grep -Eo 'Total time: .* secs')
+  return 0
+}
+
+function resultSummary(){
+  result=$1
+  timeTaken=$(extractTime "$result")
+  lines=$(echo "${result}" | grep -E -A 1 '\[server\].*wins.*')
+  summary=$(echo $lines | sed -E 's/\[server\] *//g')
+  echo "${summary} (${timeTaken})"
+  return 0
+}
+
+function buildMatchCommand(){
+  team1=$1
+  team2=$2
+  map=$3
+  echo "./gradlew --offline runQuiet -PteamA=${team1} -PteamB=${team2} -Pmaps=${map}"
+  return 0
+}
+
+function printResult(){
+  result=$1
+  me=$2
+  prepend=$3
+  cmnd=$4
+  summary=$(resultSummary "$result")
+  if [[ $summary == *${me}* ]]; then
+    echo -e "\033[0;36m  ${prepend}${summary}\033[0m"
+  else
+    echo -e "\033[0;31m  ${prepend}${summary}\033[0m"
+    echo -e "\033[0;31m     ${cmnd}\033[0m"
+  fi
+}
+
 
 echo -e "\033[0;35mStarting Tournament\033[0m"
 echo -e "\033[0;35m-------------------\033[0m"
 for o in ${OPPONENTS[@]}; do
   for m in ${MAPS[@]}; do
     echo -e "\033[0;37mVs. ${o} on ${m}\033[0m"
-    COMMAND_A="./gradlew --offline runQuiet -PteamA=swarming -PteamB=${o} -Pmaps=${m}"
-    COMMAND_B="./gradlew --offline runQuiet -PteamB=swarming -PteamA=${o} -Pmaps=${m}"
-    RESULT_A=`${COMMAND_A} | grep -E -A 1 '\[server\].*wins.*'`
-    RESULT_B=`${COMMAND_B} | grep -E -A 1 '\[server\].*wins.*'`
-    CLEAN_RESULT_A=`echo ${RESULT_A} | sed -E 's/\[server\] *//g'`
-    CLEAN_RESULT_B=`echo ${RESULT_B} | sed -E 's/\[server\] *//g'`
-    if [[ $CLEAN_RESULT_A == *${ME}* ]]; then
-      echo -e "\033[0;36m  A: ${CLEAN_RESULT_A}\033[0m"
-    else
-      echo -e "\033[0;31m  A: ${CLEAN_RESULT_A}\033[0m"
-      echo -e "\033[0;31m     ${COMMAND_A}\033[0m"
-    fi
-    if [[ $CLEAN_RESULT_B == *${ME}* ]]; then
-      echo -e "\033[0;36m  B: ${CLEAN_RESULT_B}\033[0m"
-    else
-      echo -e "\033[0;31m  B: ${CLEAN_RESULT_B}\033[0m"
-      echo -e "\033[0;31m     ${COMMAND_B}\033[0m"
-    fi
+    COMMAND_A=$(buildMatchCommand "swarming" $o $m)
+    COMMAND_B=$(buildMatchCommand $o "swarming" $m)
+    RESULT_A=$($COMMAND_A)
+    #RESULT_B=$($COMMAND_B)
+    printResult "${RESULT_A}" "$ME" "A: " "$COMMAND_A"
+    #printResult "${RESULT_B}" "$ME" "B: " "$COMMAND_B"
+    #COMMAND_A="./gradlew --offline runQuiet -PteamA=swarming -PteamB=${o} -Pmaps=${m}"
+    #COMMAND_B="./gradlew --offline runQuiet -PteamB=swarming -PteamA=${o} -Pmaps=${m}"
+    #WIN_LINES="grep -E -A 1 '\[server\].*wins.*'"
+    #TIME_GREP="grep -Eo 'Total time: .*'"
+    #SERVER_STRIP=
+    #RESULT_A=`${COMMAND_A}`
+    #RESULT_B=`${COMMAND_B}`
+    #CLEAN_RESULT_A=`echo ${RESULT_A} | ${WIN_LINES} | sed -E 's/\[server\] *//g'`
+    #CLEAN_RESULT_B=`echo ${RESULT_B} | eval ${WIN_LINES} | sed -E 's/\[server\] *//g'`
+    #TIME_A=`echo ${RESULT_A} | ${TIME_GREP}`
+    #TIME_B=`echo ${RESULT_B} | ${TIME_GREP}`
+    #if [[ $CLEAN_RESULT_A == *${ME}* ]]; then
+    #  echo -e "\033[0;36m  A: ${CLEAN_RESULT_A} (${TIME_A})\033[0m"
+    #else
+    #  echo -e "\033[0;31m  A: ${CLEAN_RESULT_A} (${TIME_A})\033[0m"
+    #  echo -e "\033[0;31m     ${COMMAND_A}\033[0m"
+    #fi
+    #if [[ $CLEAN_RESULT_B == *${ME}* ]]; then
+    #  echo -e "\033[0;36m  B: ${CLEAN_RESULT_B} (${TIME_B})\033[0m"
+    #else
+    #  echo -e "\033[0;31m  B: ${CLEAN_RESULT_B} (${TIME_B})\033[0m"
+    #  echo -e "\033[0;31m     ${COMMAND_B}\033[0m"
+    #fi
   done
 done
